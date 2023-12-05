fdd1d6a41ea746c5b96d5c4eb213f6e9b82d15f0
lex.neufeld <lex.neufeld@gmail.com>
2023-12-05 14:54:34 +0100

    Class Trivia started code to import textfile to ArrayList<Trivia>

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 # MemGlobe
 
 Short Description:
+
 Learn and stay up-to-date of Geography Trivia with spaced repetition. Establish a Mind Map of the globe with latitude and longitude. Create targeted learning goals (ex: selected demographics in a region) and generate progress reports. 
+
 The goal is easy-to-use software for mobile devices for people to learn the name and location of all countries which then enables faster learning and understanding of global issues. 
+
 Mnemonic theory suggests that learning SEVERAL pieces of interlinked or useful trivia for each country speeds up knowledge acquisition and increases retention. Therefore, user can select from a broad range of trivia to learn.
 
 Extended Description:
+
 ⦁	List of countries is stored locally, can self-generate, self-update, and is journaling. Or just download an update from a mirror. 
+
 ⦁	Advanced cross-linked spaced repetition  triggers review of related trivia. The answer to one trivia is several other trivia.
+
 ⦁	Pictures optimized for mobile devices.
+
 ⦁	Low-distortion maps always centered on subject.
+
 ⦁	Country and Group-based rules for determining which Trivia to learn. User can use pre-defined groups (such as continents, alliances, cultural or economic blocks), define new groups, and set rules for groups or individual countries.
+
 ⦁	track user progress
+
 ⦁	pronunciation guides/audio
+
 
 Trivia (subject to change)
 1.		Name
@@ -75,3 +86,4 @@ or for two contries to point to the same language, and that language to point ba
 adding/maintaining information to the database can still be done with a hierarchy when it makes sense by using templates
 Call up a trivia, and it will display all, or a pre-defined set of, the linked information as if they were members of it, but really france links to paris the same way paris links to france and neither are members of eachother, and a treaty could link back to paris without france or paris being linked to that treaty
 
+it has occured to Lex that he has no idea how a river is supposed to work. If the category is river and the id is Sein_river, but I acutally want the name of the river and the map of the river as two seperate trivia that could be used as questions or answers, what do I do?
--- src/main/java/MemGlobeApplication.java
+++ src/main/java/MemGlobeApplication.java
@@ -1,5 +1,72 @@
-public class MemGlobeApplication {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class MemGlobeApplication {
+    public static class Trivia{
+        String id; //unique name
+        Object information; //primary information to be presented: text, audio, map, picture, etc.
+        Object location; //various latitude and longitude values
+        String category; //each trivia belongs to exactly one widely accepted category of Geography Trivia such as Country, Capital, River. It probably needs to be repeated in the id such as France_Country, France_Capital, Sein_River
+        ArrayList<String> groups; //For establishing group rules. For internal logic, this is actually redundant with category, but makes the structure easier to understand. Examples of groups that could be used to filter trivia in or out include regions (Africa, EU), alliances (NATO, CSTO), landlocked, on the Danube.
+        boolean askable; //can this trivia be used as a question to other trivia?
+
+        public void addAnswer(String answer) {
+            this.answers.add(answer);
+        }
+
+        ArrayList<String> answers; //all the other Trivia.id that can be answers to this trivia
+        ArrayList<Trivia> old; //previous versions of this Trivia so you know you aren't going crazy when things change
+
+        public Trivia(String id, String category, boolean askable, ArrayList<String> answers) {
+            this.id = id;
+            this.category = category;
+            this.askable = askable;
+            this.answers = answers;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Trivia> currentSet = new ArrayList<>();
+        try{
+            Scanner myScanner = new Scanner(new File("src/main/resources/countriesAndCapitals.txt"));
+
+            //How wide is the list and what are the category names for each column?
+            String column0 = "country";
+            String column1 = "capital";
+
+
+            while(myScanner.hasNext()){
+                //get next line of file split on tab
+                String[] currentLine = myScanner.nextLine().split("\t");
+                //skip lines that have been commented out with #
+                if (currentLine[0].startsWith("#")){
+                    System.out.println(currentLine[0]);
+                    continue;
+                }
+                //create Trivia.id for each field by adding the contents of the field to "_category"
+                String currentColumn0Id = currentLine[0]+"_"+column0;
+                String currentColumn1Id = currentLine[0]+"_"+column1;
+
+                //set the answers for each field to the other fields
+                ArrayList<String> currentColumn0Answers = new ArrayList<>(Arrays.asList(currentColumn1Id));
+                ArrayList<String> currentColumn1Answers = new ArrayList<>(Arrays.asList(currentColumn0Id));
+
+                //create a new Trivia in currentSet for each field
+                currentSet.add(new Trivia(currentColumn0Id, column0, true, currentColumn0Answers));
+                currentSet.add(new Trivia(currentColumn1Id, column1, true, currentColumn1Answers));
+
+            }
+            myScanner.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("currentSet has size: " + currentSet.size());
+        System.out.println(currentSet.get(0).id);
+        System.out.println(currentSet.get(1).id);
     }
 }
